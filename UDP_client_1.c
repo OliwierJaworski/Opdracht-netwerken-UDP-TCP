f24845563b61d8756e6ff2aa8d2f954ca978ff7f
Bart-PXL <20003435@pxl.be>
2021-04-13 21:04:17 +0200

    UDP client : IPv4 & IPv6 difference

--- UDP_client_1.c
+++ UDP_client_1.c
@@ -1,4 +1,4 @@
-//
+// gcc UDP_client_1.c -lws2_32 -o UDP_client_1.exe -D IPv4
 // _WIN32_WINNT version constants --> https://stackoverflow.com/questions/15370033/how-to-use-inet-pton-with-the-mingw-compiler
 //
 // #define _WIN32_WINNT_NT4                    0x0400 // Windows NT 4.0
@@ -26,9 +26,6 @@
 #include <errno.h>
 #include <string.h>
 
-//int inet_pton( int af, const char * src, void * dst );
-//const char * inet_ntop( int af, const void * src, char * dst, socklen_t size );
-
 int main( int argc, char * argv[] )
 {
 	WSADATA wsaData; // if this doesn't work
@@ -41,27 +38,19 @@ int main( int argc, char * argv[] )
 		exit( 1 );
 	}
 
-
-/*
-	struct sockaddr_in internet_address;
-	internet_address.sin_family = AF_INET; //IPv4 == AF_INET ; IPv6 == AF_INET6
-	internet_address.sin_port = htons( 24042 ); //Host to Network Short (byte order)
-	internet_address.sin_addr.s_addr = inet_addr( "127.0.0.1" ); //internet
-	memset( internet_address.sin_zero, '\0', sizeof internet_address.sin_zero );
-*/
-
+#ifdef IPv4
 	struct sockaddr_in internet_address;
 	
 	internet_address.sin_family = AF_INET;
 	internet_address.sin_port = htons( 24042 ); //Host to Network Short (byte order)
 	inet_pton( AF_INET, "127.0.0.1", &internet_address.sin_addr );
-/*
+#else
 	struct sockaddr_in6 internet_address;
 	
-	internet_address.sin_family = AF_INET6;
-	internet_address.sin_port = htons( 24042 ); //Host to Network Short (byte order)
+	internet_address.sin6_family = AF_INET6;
+	internet_address.sin6_port = htons( 24042 ); //Host to Network Short (byte order)
 	inet_pton( AF_INET6, "::1", &internet_address.sin6_addr );
-*/
+#endif
 
 	int internet_socket;
 	
