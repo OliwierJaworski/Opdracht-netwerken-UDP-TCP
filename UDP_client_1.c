71c134909f15a09dbfb8f70f442f3dd4a3dc4b94
Bart-PXL <20003435@pxl.be>
2021-04-13 21:48:05 +0200

    UDP client : IPv6 Windows bug

--- UDP_client_1.c
+++ UDP_client_1.c
@@ -19,6 +19,7 @@
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -40,23 +41,42 @@ int main( int argc, char * argv[] )
 
 #ifdef IPv4
 	struct sockaddr_in internet_address;
+	memset( &internet_address, '\0', sizeof internet_address );
 	
 	internet_address.sin_family = AF_INET;
 	internet_address.sin_port = htons( 24042 ); //Host to Network Short (byte order)
 	inet_pton( AF_INET, "127.0.0.1", &internet_address.sin_addr );
-#else
-	struct sockaddr_in6 internet_address;
-	
-	internet_address.sin6_family = AF_INET6;
-	internet_address.sin6_port = htons( 24042 ); //Host to Network Short (byte order)
-	inet_pton( AF_INET6, "::1", &internet_address.sin6_addr );
-#endif
 
 	int internet_socket;
 	
 	internet_socket = socket( PF_INET, SOCK_DGRAM, 0 ); //UDP == SOCK_DGRAM ; TCP == SOCK_STREAM
+#else
+	struct sockaddr_in6 internet_address;
+	memset( &internet_address, '\0', sizeof internet_address ); //https://stackoverflow.com/questions/25703798/getting-error-10049-address-not-avai-on-sendto-in-udp-connection-but-bind
+	
+	internet_address.sin6_family = AF_INET6;
+	internet_address.sin6_port = htons( 24042 ); //Host to Network Short (byte order)
+	inet_pton( AF_INET6, "::1", &internet_address.sin6_addr );
 
-	sendto( internet_socket, "Hello UDP world!", 16, 0, (struct sockaddr *) &internet_address, sizeof internet_address );
+	int internet_socket;
+	
+	internet_socket = socket( PF_INET6, SOCK_DGRAM, 0 ); //https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
+#endif
+
+	if( internet_socket == -1 )
+	{
+		perror( "socket" );
+	}
+
+	int number_of_bytes_send = 0;
+	
+	number_of_bytes_send = sendto( internet_socket, "Hello UDP world!", 16, 0, (struct sockaddr *) &internet_address, sizeof internet_address );
+	
+	if( number_of_bytes_send == -1 )
+	{
+		printf( "errno = %d\n", WSAGetLastError() ); //https://docs.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
+		perror( "sendto" );
+	}
 
 	close( internet_socket );
 
