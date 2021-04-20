c130824bdbd96e9e5d433c9d4d7a6b1641341d23
Bart-PXL <20003435@pxl.be>
2021-04-20 20:57:56 +0200

    Linux : step 3

---
+++ linux/3_UDP_client.c
@@ -0,0 +1,89 @@
+#include <sys/socket.h> //for sockaddr, socket, socket
+#include <sys/types.h> //for size_t
+#include <netdb.h> //for getaddrinfo
+#include <unistd.h> //for close
+#include <netinet/in.h> //for sockaddr_in
+#include <arpa/inet.h> //for htons, htonl, inet_pton, inet_ntop
+#include <string.h> //for memset
+#include <stdio.h> //for fprintf, perror
+#include <errno.h> //for errno
+
+void print_ip_address( struct addrinfo * ip )
+{
+	void * ip_address;
+	char * ip_version;
+	char ip_string[INET6_ADDRSTRLEN];
+
+	if( ip->ai_family == AF_INET )
+	{ // IPv4
+		struct sockaddr_in *ipv4 = (struct sockaddr_in *)ip->ai_addr;
+		ip_address = &(ipv4->sin_addr);
+		ip_version = "IPv4";
+	}
+	else
+	{ // IPv6
+		struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)ip->ai_addr;
+		ip_address = &(ipv6->sin6_addr);
+		ip_version = "IPv6";
+	}
+
+	inet_ntop( ip->ai_family, ip_address, ip_string, sizeof ip_string );
+	printf( "%s -> %s\n", ip_version, ip_string );
+}
+
+int main( int argc, char * argv[] )
+{
+	struct addrinfo internet_address_setup, *result_head, *result_item;
+	memset( &internet_address_setup, 0, sizeof internet_address_setup );
+	internet_address_setup.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+	internet_address_setup.ai_socktype = SOCK_DGRAM;
+
+	int getaddrinfo_return;
+	getaddrinfo_return = getaddrinfo( "localhost.pxl-ea-ict.be", "24042", &internet_address_setup, &result_head );
+	if( getaddrinfo_return != 0 )
+	{
+		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
+		exit( 2 );
+	}
+
+	struct sockaddr * internet_address;
+	size_t internet_address_length;
+	int internet_socket;
+
+	result_item = result_head; //take first of the linked list
+	while( result_item != NULL ) //while the pointer is valid
+	{
+		internet_socket = socket( result_item->ai_family, result_item->ai_socktype, result_item->ai_protocol );
+		if( internet_socket == -1 )
+		{
+			perror( "socket" );
+		}
+		else
+		{
+			printf( "Sending to " );
+			print_ip_address( result_item );
+			internet_address_length = result_item->ai_addrlen;
+			internet_address = (struct sockaddr*) malloc( internet_address_length );
+			memcpy( internet_address, result_item->ai_addr, internet_address_length );
+			break; //stop running through the linked list
+		}
+		result_item = result_item->ai_next; //take next in the linked list
+	}
+	if( result_item == NULL )
+	{
+		fprintf( stderr, "socket: no valid socket address found\n" );
+		exit( 3 );
+	}
+	freeaddrinfo( result_head ); //free the linked list
+
+	int number_of_bytes_send = 0;
+	number_of_bytes_send = sendto( internet_socket, "Hello UDP world!", 16, 0, internet_address, internet_address_length );
+	if( number_of_bytes_send == -1 )
+	{
+		perror( "sendto" );
+	}
+
+	close( internet_socket );
+
+	return 0;
+}
