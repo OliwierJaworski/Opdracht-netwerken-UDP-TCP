5cb09c5096f2e168e5432b567e3c32f4cd8bb685
Bart-PXL <20003435@pxl.be>
2021-04-21 22:01:17 +0200

    TCP client : clean

--- windows/7_TCP_client.c
+++ windows/7_TCP_client.c
@@ -1,4 +1,4 @@
-// gcc 6_TCP_client.c -lws2_32 -o 6_TCP_client.exe -D IPv4
+// gcc 7_TCP_client.c -lws2_32 -o 7_TCP_client.exe
 // _WIN32_WINNT version constants --> https://stackoverflow.com/questions/15370033/how-to-use-inet-pton-with-the-mingw-compiler
 //
 // #define _WIN32_WINNT_NT4                    0x0400 // Windows NT 4.0
@@ -23,6 +23,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void print_ip_address( struct addrinfo * ip )
+{
+	void * ip_address;
+	char * ip_version;
+	char ip_string[INET6_ADDRSTRLEN];
+
+	if( ip->ai_family == AF_INET )
+	{ // IPv4
+		struct sockaddr_in *ipv4 = (struct sockaddr_in *)ip->ai_addr;
+		ip_address = &(ipv4->sin_addr);
+		ip_version = "IPv4";
+	}
+	else
+	{ // IPv6
+		struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)ip->ai_addr;
+		ip_address = &(ipv6->sin6_addr);
+		ip_version = "IPv6";
+	}
+
+	inet_ntop( ip->ai_family, ip_address, ip_string, sizeof ip_string );
+	printf( "%s -> %s\n", ip_version, ip_string );
+}
+
 int main( int argc, char * argv[] )
 {
 	WSADATA wsaData; //WSAData wsaData; //Could be different case
@@ -32,74 +55,57 @@ int main( int argc, char * argv[] )
 		exit( 1 );
 	}
 
-#ifdef IPv4
+	struct addrinfo internet_address_setup, *result_head, *result_item;
+	memset( &internet_address_setup, 0, sizeof internet_address_setup );
+	internet_address_setup.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+	internet_address_setup.ai_socktype = SOCK_STREAM;
 
-	struct sockaddr_in internet_address;
-	memset( &internet_address, '\0', sizeof internet_address );
-	internet_address.sin_family = AF_INET;
-	internet_address.sin_port = htons( 24042 ); //Host to Network Short (byte order)
-	internet_address.sin_addr.s_addr = htonl( INADDR_LOOPBACK ); //INADDR_LOOPBACK == localhost
-
-/*	int inet_pton_return;
-	inet_pton_return = inet_pton( AF_INET, "127.0.0.1", &internet_address.sin_addr );
-	if( inet_pton_return < 1 )
+	int getaddrinfo_return;
+	getaddrinfo_return = getaddrinfo( "localhost.pxl-ea-ict.be", "24042", &internet_address_setup, &result_head );
+	if( getaddrinfo_return != 0 )
 	{
-		if( inet_pton_return == 0 )
-		{
-			fprintf( stderr, "Bad IP format" );
-		}
-		else
-		{
-			perror( "inet_pton" );
-		}
+		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
 		exit( 2 );
-	}*/
-
-	int internet_socket;
-	internet_socket = socket( PF_INET, SOCK_STREAM, 0 ); //UDP == SOCK_DGRAM ; TCP == SOCK_STREAM
-
-#else
-
-	struct sockaddr_in6 internet_address;
-	memset( &internet_address, '\0', sizeof internet_address ); //https://stackoverflow.com/questions/25703798/getting-error-10049-address-not-avai-on-sendto-in-udp-connection-but-bind
-	internet_address.sin6_family = AF_INET6;
-	internet_address.sin6_port = htons( 24042 ); //Host to Network Short (byte order)
-	internet_address.sin6_addr = in6addr_loopback; //in6addr_any == :: ; in6addr_loopback == ::1
-
-	/*int inet_pton_return;
-	inet_pton_return = inet_pton( AF_INET6, "::1", &internet_address.sin6_addr );
-	if( inet_pton_return < 1 )
-	{
-		if( inet_pton_return == 0 )
-		{
-			fprintf( stderr, "Bad IP format" );
-		}
-		else
-		{
-			perror( "inet_pton" );
-		}
-		exit( 2 );
-	}*/
-
-	int internet_socket;
-	internet_socket = socket( PF_INET6, SOCK_STREAM, 0 ); //https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
-
-#endif
-
-	if( internet_socket == -1 )
-	{
-		perror( "socket" );
-		exit( 3 );
 	}
 
-	int connect_return;
-	connect_return = connect( internet_socket, (struct sockaddr *) &internet_address, sizeof internet_address );
-	if( connect_return == -1 )
+	struct sockaddr * internet_address;
+	size_t internet_address_length;
+	int internet_socket;
+
+	result_item = result_head; //take first of the linked list
+	while( result_item != NULL ) //while the pointer is valid
 	{
-		perror( "connect" );
-		close( internet_socket );
+		internet_socket = socket( result_item->ai_family, result_item->ai_socktype, result_item->ai_protocol );
+		if( internet_socket == -1 )
+		{
+			perror( "socket" );
+		}
+		else
+		{
+			printf( "Connecting to " );
+			print_ip_address( result_item );
+
+			int connect_return;
+			connect_return = connect( internet_socket, result_item->ai_addr, result_item->ai_addrlen );
+			if( connect_return == -1 )
+			{
+				perror( "connect" );
+				close( internet_socket );
+			}
+			else
+			{
+				printf( "Connected\n" );
+				break; //stop running through the linked list
+			}
+		}
+		result_item = result_item->ai_next; //take next in the linked list
+	}
+	if( result_item == NULL )
+	{
+		fprintf( stderr, "socket: no valid socket address found\n" );
 		exit( 4 );
 	}
+	freeaddrinfo( result_head ); //free the linked list
 
 	int number_of_bytes_send = 0;
 	number_of_bytes_send = send( internet_socket, "Hello TCP world!", 16, 0 );
