e789fb1a4eb41c2a9f4e853a6f97441fdd48e896
Bart-PXL <20003435@pxl.be>
2021-05-08 22:50:55 +0200

    Step by step

--- 6_UDP_server.c
+++ 6_UDP_server.c
@@ -36,9 +36,9 @@
 	int OSCleanup( void ) {}
 #endif
 
-int initialization( struct sockaddr ** internet_address, socklen_t * internet_address_length );
-void execution( int internet_socket, struct sockaddr * internet_address, socklen_t internet_address_length );
-void cleanup( int internet_socket, struct sockaddr * internet_address );
+int initialization();
+void execution( int internet_socket );
+void cleanup( int internet_socket );
 
 int main( int argc, char * argv[] )
 {
@@ -48,29 +48,27 @@ int main( int argc, char * argv[] )
 
 	OSInit();
 
-	struct sockaddr * internet_address = NULL;
-	socklen_t internet_address_length = 0;
-	int internet_socket = initialization( &internet_address, &internet_address_length );
+	int internet_socket = initialization();
 
 	/////////////
 	//Execution//
 	/////////////
 
-	execution( internet_socket, internet_address, internet_address_length );
+	execution( internet_socket );
 
 
 	////////////
 	//Clean up//
 	////////////
 
-	cleanup( internet_socket, internet_address );
+	cleanup( internet_socket );
 
 	OSCleanup();
 
 	return 0;
 }
 
-int initialization( struct sockaddr ** internet_address, socklen_t * internet_address_length )
+int initialization()
 {
 	//Step 1.1
 	struct addrinfo internet_address_setup;
@@ -78,7 +76,8 @@ int initialization( struct sockaddr ** internet_address, socklen_t * internet_ad
 	memset( &internet_address_setup, 0, sizeof internet_address_setup );
 	internet_address_setup.ai_family = AF_UNSPEC;
 	internet_address_setup.ai_socktype = SOCK_DGRAM;
-	int getaddrinfo_return = getaddrinfo( "127.0.0.1", "24042", &internet_address_setup, &internet_address_result );
+	internet_address_setup.ai_flags = AI_PASSIVE;
+	int getaddrinfo_return = getaddrinfo( NULL, "24042", &internet_address_setup, &internet_address_result );
 	if( getaddrinfo_return != 0 )
 	{
 		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
@@ -98,10 +97,16 @@ int initialization( struct sockaddr ** internet_address, socklen_t * internet_ad
 		else
 		{
 			//Step 1.3
-			*internet_address_length = internet_address_result_iterator->ai_addrlen;
-			*internet_address = (struct sockaddr *) malloc( internet_address_result_iterator->ai_addrlen );
-			memcpy( *internet_address, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
-			break;
+			int bind_return = bind( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
+			if( bind_return == -1 )
+			{
+				close( internet_socket );
+				perror( "bind" );
+			}
+			else
+			{
+				break;
+			}
 		}
 		internet_address_result_iterator = internet_address_result_iterator->ai_next;
 	}
@@ -117,20 +122,14 @@ int initialization( struct sockaddr ** internet_address, socklen_t * internet_ad
 	return internet_socket;
 }
 
-void execution( int internet_socket, struct sockaddr * internet_address, socklen_t internet_address_length )
+void execution( int internet_socket )
 {
 	//Step 2.1
-	int number_of_bytes_send = 0;
-	number_of_bytes_send = sendto( internet_socket, "Hello UDP world!", 16, 0, internet_address, internet_address_length );
-	if( number_of_bytes_send == -1 )
-	{
-		perror( "sendto" );
-	}
-
-	//Step 2.2
 	int number_of_bytes_received = 0;
 	char buffer[1000];
-	number_of_bytes_received = recvfrom( internet_socket, buffer, ( sizeof buffer ) - 1, 0, internet_address, &internet_address_length );
+	struct sockaddr_storage client_internet_address;
+	socklen_t client_internet_address_length = sizeof client_internet_address;
+	number_of_bytes_received = recvfrom( internet_socket, buffer, ( sizeof buffer ) - 1, 0, (struct sockaddr *) &client_internet_address, &client_internet_address_length );
 	if( number_of_bytes_received == -1 )
 	{
 		perror( "recvfrom" );
@@ -140,13 +139,18 @@ void execution( int internet_socket, struct sockaddr * internet_address, socklen
 		buffer[number_of_bytes_received] = '\0';
 		printf( "Received : %s\n", buffer );
 	}
+
+	//Step 2.2
+	int number_of_bytes_send = 0;
+	number_of_bytes_send = sendto( internet_socket, "Hello UDP world!", 16, 0, (struct sockaddr *) &client_internet_address, client_internet_address_length );
+	if( number_of_bytes_send == -1 )
+	{
+		perror( "sendto" );
+	}
 }
 
-void cleanup( int internet_socket, struct sockaddr * internet_address )
+void cleanup( int internet_socket )
 {
-	//Step 3.2
-	free( internet_address );
-
 	//Step 3.1
 	close( internet_socket );
 }
